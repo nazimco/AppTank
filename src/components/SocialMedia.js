543c55814ed7ce2831ebf0850e2ce6322f615b4b
stephen112233 <applejuicekid@gmail.com>
2018-06-26 13:08:38 -0400

    twitter

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
 import '../css/testcss.css';
 
+import { Timeline } from 'react-twitter-widgets'
+// var Timeline = require('react-twitter-widgets').Timeline
+ 
+// ReactDOM.render((
+//   <Timeline
+//     dataSource={{
+//       sourceType: 'profile',
+//       screenName: 'twitterdev'
+//     }}
+//     options={{
+//       username: 'TwitterDev',
+//       height: '400'
+//     }}
+//     onLoad={() => console.log('Timeline is loaded!')}
+//   />
+// ), document.getElementById('root'))
+
 
 
 class SocialMedia extends Component {
   render() {
     return (
       <div id="socialmedia">
+//   <Timeline
+    dataSource={{
+      sourceType: 'profile',
+      screenName: 'twitterdev'
+    }}
+    options={{
+      username: 'DJKhaled',
+      height: '400'
+    }}
+    onLoad={() => console.log('Timeline is loaded!')}
+  />
+       
 
-        <div className="instagram">
-          <h2> Instagram </h2>
-          
-
-        </div>
-
-        <div className="snapchat">
-          <h2> Snapchat </h2>
-        </div>
-
+      
         <div className="twitter">
           <h2> Twitter </h2>
-          <a class="twitter-timeline" href="https://twitter.com/djkhaled?ref_src=twsrc%5Etfw">
-        Tweets by djkhaled</a>
+        
           
         </div>
 
