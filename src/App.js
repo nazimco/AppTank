8ab4a21d4f7af4ed91f1eb2f321523d64cc25f59
aenuros <37673253+aenuros@users.noreply.github.com>
2018-06-27 16:28:59 -0400

    Replace every instance of Video with Itunes
    Change music to 'stream music'

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SocialMedia from './components/SocialMedia';
 import Music from './components/Music';
 import Shop from './components/Shop';
 import About from './components/About';
-import Video from './components/Video';
+import Itunes from './components/Itunes';
 
 
 
@@ -18,7 +18,7 @@ constructor(){
     nolineHome: "line",
     nolineAbout: "noline",
     nolineMusic: "noline",
-    nolineVideo: "noline",
+    nolineItunes: "noline",
     nolineTwitter: "noline",
     nolineShop: "noline",
   }
@@ -33,8 +33,8 @@ handleClickAbout() {
 handleClickMusic() {
   this.toggleMenu("nolineMusic")
 }
-handleClickVideo() {
-  this.toggleMenu("nolineVideo")
+handleClickItunes() {
+  this.toggleMenu("nolineItunes")
 }
 handleClickTwitter() {
   this.toggleMenu("nolineTwitter")
@@ -48,7 +48,7 @@ toggleMenu(item){
     nolineHome: "noline",
     nolineAbout: "noline",
     nolineMusic: "noline",
-    nolineVideo: "noline",
+    nolineItunes: "noline",
     nolineTwitter: "noline",
     nolineShop: "noline",
   };
@@ -64,7 +64,7 @@ toggleMenu(item){
 // let line_class_home = this.state.nolineHome ? "noline" : "line";
 // let line_class_about = this.state.nolineAbout ? "noline" : "line";
 // let line_class_music = this.state.nolineMusic ? "noline" : "line";
-// let line_class_video = this.state.nolineVideo ? "noline" : "line";
+// let line_class_itunes = this.state.nolineItunes ? "noline" : "line";
 // let line_class_twitter = this.state.nolineTwitter ? "noline" : "line";
 // let line_class_shop = this.state.nolineShop ? "noline" : "line";
 
@@ -74,8 +74,8 @@ toggleMenu(item){
         <div className="NavBar">
           <Link className={this.state.nolineHome} onClick={this.handleClickHome.bind(this)}   to="/home">Home</Link>
           <Link className={this.state.nolineAbout} onClick={this.handleClickAbout.bind(this)} to="/about">About</Link>
-          <Link className={this.state.nolineMusic}  onClick={this.handleClickMusic.bind(this)} to="/music">Music</Link>
-          <Link  className={this.state.nolineVideo}   onClick={this.handleClickVideo.bind(this)} to="/video">Video</Link>
+          <Link className={this.state.nolineMusic}  onClick={this.handleClickMusic.bind(this)} to="/music">Stream Music</Link>
+          <Link  className={this.state.nolineItunes}   onClick={this.handleClickItunes.bind(this)} to="/Itunes">Buy Music</Link>
           <Link className={this.state.nolineTwitter}   onClick={this.handleClickTwitter.bind(this)} to="/socialmedia">Twitter</Link>
           <Link  className={this.state.nolineShop}  onClick={this.handleClickShop.bind(this)}  to="/shop">Shop</Link>
         </div>
@@ -83,7 +83,7 @@ toggleMenu(item){
 
           <Route path="/music" component={Music} />
           <Route path="/home" component={Home} />
-          <Route path="/video" component={Video} />
+          <Route path="/itunes" component={Itunes} />
           <Route path="/socialmedia" component={SocialMedia} />
           <Route path="/about" component={About} />
           <Route path="/shop" component={Shop} />
